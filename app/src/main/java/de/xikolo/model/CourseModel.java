77c8f7ef9f8c4d6e3f22f1290a0e161c76eec5b0
Tobias Rohloff <tobias.rohloff@student.hpi.uni-potsdam.de>
2015-01-21 20:41:47 +0100

    Add offline mode for all models.

--- app/src/main/java/de/xikolo/model/CourseModel.java
+++ app/src/main/java/de/xikolo/model/CourseModel.java
@@ -1,126 +1,49 @@
 package de.xikolo.model;
 
 import android.content.Context;
-import android.os.Handler;
-import android.os.Looper;
 
 import com.path.android.jobqueue.JobManager;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import de.xikolo.data.preferences.EnrollmentsPreferences;
+import de.xikolo.data.database.CourseDataAccess;
+import de.xikolo.data.database.DatabaseHelper;
+import de.xikolo.data.database.ModuleDataAccess;
 import de.xikolo.data.entities.Course;
 import de.xikolo.model.jobs.CreateEnrollmentJob;
 import de.xikolo.model.jobs.DeleteEnrollmentJob;
-import de.xikolo.model.jobs.OnJobResponseListener;
 import de.xikolo.model.jobs.RetrieveCoursesJob;
 
 public class CourseModel extends BaseModel {
 
     public static final String TAG = CourseModel.class.getSimpleName();
 
-    public static final String FILTER_ALL = "filter_all";
-    public static final String FILTER_MY = "filter_my";
+    private CourseDataAccess courseDataAccess;
+    private ModuleDataAccess moduleDataAccess;
 
-    private OnModelResponseListener<List<Course>> mListener;
-
-    private EnrollmentsPreferences mEnrollmentPref;
-
-    public CourseModel(Context context, JobManager jobManager) {
+    public CourseModel(Context context, JobManager jobManager, DatabaseHelper databaseHelper) {
         super(context, jobManager);
 
-        this.mEnrollmentPref = new EnrollmentsPreferences(context);
+        courseDataAccess = new CourseDataAccess(databaseHelper);
+        moduleDataAccess = new ModuleDataAccess(databaseHelper);
     }
 
-    public static int readEnrollmentsSize(Context context) {
-        EnrollmentsPreferences prefs = new EnrollmentsPreferences(context);
-        return prefs.getEnrollmentsSize();
+    public int getEnrollmentsCount() {
+        return courseDataAccess.getEnrollmentsCount();
     }
 
-    public static void deleteEnrollmentsSize(Context context) {
-        EnrollmentsPreferences prefs = new EnrollmentsPreferences(context);
-        prefs.deleteEnrollmentsSize();
+    public void getCourses(Result<List<Course>> result, boolean includeProgress) {
+        mJobManager.addJobInBackground(new RetrieveCoursesJob(result, includeProgress, courseDataAccess));
     }
 
-    public void retrieveCourses(boolean cache, boolean includeProgress) {
-        retrieveCourses(FILTER_ALL, cache, includeProgress);
+    public void addEnrollment(Result<Void> result, Course course) {
+        mJobManager.addJobInBackground(new CreateEnrollmentJob(result, course, courseDataAccess));
     }
 
-    public void retrieveCourses(final String filter, boolean cache, boolean includeProgress) {
-        OnJobResponseListener<List<Course>> callback = new OnJobResponseListener<List<Course>>() {
-            @Override
-            public void onResponse(final List<Course> response) {
-                final List<Course> myCourses = new ArrayList<Course>();
-                if (response != null) {
-                    for (Course course : response) {
-                        if (course.is_enrolled) {
-                            myCourses.add(course);
-                        }
-                    }
-                    mEnrollmentPref.saveEnrollmentsSize(myCourses.size());
-                }
-                if (mListener != null) {
-                    new Handler(Looper.getMainLooper()).post(new Runnable() {
-                        @Override
-                        public void run() {
-                            if (FILTER_MY.equals(filter)) {
-                                mListener.onResponse(myCourses);
-                            } else {
-                                mListener.onResponse(response);
-                            }
-                        }
-                    });
-                }
-            }
+    public void deleteEnrollment(Result<Void> result, Course course) {
+        mJobManager.addJobInBackground(new DeleteEnrollmentJob(result, course, courseDataAccess, moduleDataAccess));
 
-            @Override
-            public void onCancel() {
-                if (mListener != null) {
-                    new Handler(Looper.getMainLooper()).post(new Runnable() {
-                        @Override
-                        public void run() {
-                            mListener.onResponse(null);
-                        }
-                    });
-                }
-            }
-        };
-        mJobManager.addJobInBackground(new RetrieveCoursesJob(callback, cache, includeProgress, UserModel.readAccessToken(mContext)));
-    }
 
-    public void setRetrieveCoursesListener(OnModelResponseListener<List<Course>> listener) {
-        mListener = listener;
-    }
-
-    public void createEnrollment(String enrollmentId) {
-        OnJobResponseListener<Void> callback = new OnJobResponseListener<Void>() {
-            @Override
-            public void onResponse(Void response) {
-                retrieveCourses(false, false);
-            }
-
-            @Override
-            public void onCancel() {
-
-            }
-        };
-        mJobManager.addJobInBackground(new CreateEnrollmentJob(callback, enrollmentId, UserModel.readAccessToken(mContext)));
-    }
-
-    public void deleteEnrollment(String enrollmentId) {
-        OnJobResponseListener<Void> callback = new OnJobResponseListener<Void>() {
-            @Override
-            public void onResponse(Void response) {
-                retrieveCourses(false, false);
-            }
-
-            @Override
-            public void onCancel() {
-
-            }
-        };
-        mJobManager.addJobInBackground(new DeleteEnrollmentJob(callback, enrollmentId, UserModel.readAccessToken(mContext)));
     }
 
 }
