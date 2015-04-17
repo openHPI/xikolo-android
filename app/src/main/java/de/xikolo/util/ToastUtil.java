f562171f8fad5e10e2dadcce772bb68f5023947d
Tobias Rohloff <tobias.rohloff@student.hpi.uni-potsdam.de>
2015-04-17 12:53:21 +0200

    Item tabs show title on long press.

--- app/src/main/java/de/xikolo/util/ToastUtil.java
+++ app/src/main/java/de/xikolo/util/ToastUtil.java
@@ -1,6 +1,7 @@
 package de.xikolo.util;
 
 import android.content.Context;
+import android.view.Gravity;
 import android.widget.Toast;
 
 public class ToastUtil {
@@ -11,6 +12,18 @@ public class ToastUtil {
 
     public static void show(Context context, int stringId) {
         Toast.makeText(context, context.getString(stringId), Toast.LENGTH_SHORT).show();
+    }
+
+    public static void show(Context context, String message, int gravity, int xOffset, int yOffset) {
+        Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+        toast.setGravity(gravity, xOffset, yOffset);
+        toast.show();
+    }
+
+    public static void show(Context context, int stringId, int gravity, int xOffset, int yOffset) {
+        Toast toast = Toast.makeText(context, context.getString(stringId), Toast.LENGTH_SHORT);
+        toast.setGravity(gravity, xOffset, yOffset);
+        toast.show();
     }
 
 }
