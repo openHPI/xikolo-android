4a08efb3625da048fdd4afa039831401ad2f4457
Tobias Rohloff <tobias.rohloff@student.hpi.uni-potsdam.de>
2015-05-06 12:18:15 +0200

    Open LTI Items in default browser and log in user.

--- app/src/main/java/de/xikolo/controller/module/WebItemFragment.java
+++ app/src/main/java/de/xikolo/controller/module/WebItemFragment.java
@@ -82,7 +82,8 @@ public class WebItemFragment<T extends ItemDetail> extends PagerFragment<T> {
         mWebViewController = new WebViewController(getActivity(), layout);
 
         if (mItem.type.equals(Item.TYPE_TEXT)
-                || mItem.type.equals(Item.TYPE_VIDEO)) {
+                || mItem.type.equals(Item.TYPE_VIDEO)
+                || mItem.type.equals(Item.TYPE_LTI)) {
             mWebViewController.setInAppLinksEnabled(false);
             mWebViewController.setLoadExternalUrlEnabled(false);
         } else if (mItem.type.equals(Item.TYPE_SELFTEST)
@@ -91,9 +92,6 @@ public class WebItemFragment<T extends ItemDetail> extends PagerFragment<T> {
                 || mItem.type.equals(Item.TYPE_PEER)) {
             mWebViewController.setInAppLinksEnabled(true);
             mWebViewController.setLoadExternalUrlEnabled(false);
-        } else if (mItem.type.equals(Item.TYPE_LTI)) {
-            mWebViewController.setInAppLinksEnabled(true);
-            mWebViewController.setLoadExternalUrlEnabled(true);
         }
 
         if (savedInstanceState != null) {
