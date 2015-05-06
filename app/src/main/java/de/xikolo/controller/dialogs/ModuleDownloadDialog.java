8100f56f448bfc573c2d9c715bcf054968ef8126
Tobias Rohloff <tobias.rohloff@student.hpi.uni-potsdam.de>
2015-05-06 19:12:46 +0200

    Add dialog to download all items for module.

---
+++ app/src/main/java/de/xikolo/controller/dialogs/ModuleDownloadDialog.java
@@ -0,0 +1,116 @@
+package de.xikolo.controller.dialogs;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+import de.xikolo.R;
+
+public class ModuleDownloadDialog extends DialogFragment {
+
+    public static final String TAG = ModuleDownloadDialog.class.getSimpleName();
+
+    private ModuleDownloadDialogListener mListener;
+
+    private boolean hdVideo;
+
+    private boolean sdVideo;
+
+    private boolean slides;
+
+    private String moduleTitle;
+
+    public static final String KEY_MODULE_TITLE = "module_title";
+
+    public static final String KEY_HD_VIDEO = "hd_video";
+    public static final String KEY_SD_VIDEO = "sd_video";
+    public static final String KEY_SLIDES = "slides";
+
+    public static ModuleDownloadDialog getInstance(String moduleTitle) {
+        ModuleDownloadDialog fragment = new ModuleDownloadDialog();
+        Bundle args = new Bundle();
+        args.putString(KEY_MODULE_TITLE, moduleTitle);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public ModuleDownloadDialog() {
+
+    }
+
+    public void setModuleDownloadDialogListener(ModuleDownloadDialogListener listener) {
+        mListener = listener;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        if (getArguments() != null) {
+            moduleTitle = getArguments().getString(KEY_MODULE_TITLE);
+        }
+        if (savedInstanceState != null) {
+            hdVideo = savedInstanceState.getBoolean(KEY_HD_VIDEO);
+            sdVideo = savedInstanceState.getBoolean(KEY_SD_VIDEO);
+            slides = savedInstanceState.getBoolean(KEY_SLIDES);
+        } else {
+            hdVideo = false;
+            sdVideo = false;
+            slides = false;
+        }
+
+        String title = String.format(getActivity().getString(R.string.dialog_module_downloads_title), moduleTitle);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(title)
+                .setMultiChoiceItems(R.array.dialog_module_downloads_array, null, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        switch (which) {
+                            case 0:
+                                hdVideo = isChecked;
+                                break;
+                            case 1:
+                                sdVideo = isChecked;
+                                break;
+                            case 2:
+                                slides = isChecked;
+                                break;
+                        }
+                    }
+                })
+                .setPositiveButton(R.string.download, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (mListener != null) {
+                            mListener.onDialogPositiveClick(ModuleDownloadDialog.this, hdVideo, sdVideo, slides);
+                        }
+                    }
+                })
+                .setNegativeButton(R.string.dialog_negative, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ModuleDownloadDialog.this.getDialog().cancel();
+                    }
+                })
+                .setCancelable(true);
+
+        AlertDialog dialog = builder.create();
+        dialog.setCanceledOnTouchOutside(true);
+
+        return dialog;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(KEY_HD_VIDEO, hdVideo);
+        outState.putBoolean(KEY_SD_VIDEO, sdVideo);
+        outState.putBoolean(KEY_SLIDES, slides);
+    }
+
+    public interface ModuleDownloadDialogListener {
+        public void onDialogPositiveClick(DialogFragment dialog, boolean hdVideo, boolean sdVideo, boolean slides);
+    }
+
+}
---
+++ app/src/main/java/de/xikolo/controller/helper/ModuleDownloadController.java
@@ -0,0 +1,120 @@
+package de.xikolo.controller.helper;
+
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+
+import de.xikolo.GlobalApplication;
+import de.xikolo.controller.dialogs.MobileDownloadDialog;
+import de.xikolo.controller.dialogs.ModuleDownloadDialog;
+import de.xikolo.controller.dialogs.ProgressDialog;
+import de.xikolo.data.entities.Course;
+import de.xikolo.data.entities.Item;
+import de.xikolo.data.entities.Module;
+import de.xikolo.data.entities.VideoItemDetail;
+import de.xikolo.data.preferences.AppPreferences;
+import de.xikolo.model.DownloadModel;
+import de.xikolo.model.ItemModel;
+import de.xikolo.model.Result;
+import de.xikolo.util.NetworkUtil;
+
+public class ModuleDownloadController {
+
+    private FragmentActivity activity;
+    
+    private boolean sdVideo;
+
+    private boolean hdVideo;
+
+    private boolean slides;
+
+    private GlobalApplication app;
+
+    private DownloadModel downloadModel;
+
+    public ModuleDownloadController(FragmentActivity activity) {
+        this.app = GlobalApplication.getInstance();
+        this.activity = activity;
+        this.downloadModel = new DownloadModel(app, app.getJobManager());
+    }
+
+    public void initModuleDownloads(final Course course, final Module module) {
+        ModuleDownloadDialog listDialog = ModuleDownloadDialog.getInstance(module.name);
+        listDialog.setModuleDownloadDialogListener(new ModuleDownloadDialog.ModuleDownloadDialogListener() {
+            @Override
+            public void onDialogPositiveClick(DialogFragment dialog, boolean hdVideo, boolean sdVideo, boolean slides) {
+                ModuleDownloadController.this.hdVideo = hdVideo;
+                ModuleDownloadController.this.sdVideo = sdVideo;
+                ModuleDownloadController.this.slides = slides;
+
+                if (hdVideo || sdVideo || slides) {
+                    if (NetworkUtil.isOnline(GlobalApplication.getInstance())) {
+                        if (NetworkUtil.getConnectivityStatus(app) == NetworkUtil.TYPE_MOBILE &&
+                                AppPreferences.isDownloadNetworkLimitedOnMobile(app)) {
+                            MobileDownloadDialog permissionDialog = MobileDownloadDialog.getInstance();
+                            permissionDialog.setMobileDownloadDialogListener(new MobileDownloadDialog.MobileDownloadDialogListener() {
+                                @Override
+                                public void onDialogPositiveClick(DialogFragment dialog) {
+                                    AppPreferences.setIsDownloadNetworkLimitedOnMobile(app, false);
+                                    startModuleDownloads(course, module);
+                                }
+                            });
+                            permissionDialog.show(activity.getSupportFragmentManager(), MobileDownloadDialog.TAG);
+                        } else {
+                            startModuleDownloads(course, module);
+                        }
+                    } else {
+                        NetworkUtil.showNoConnectionToast(GlobalApplication.getInstance());
+                    }
+                }
+            }
+        });
+        listDialog.show(activity.getSupportFragmentManager(), ModuleDownloadDialog.TAG);
+    }
+    
+    private void startModuleDownloads(final Course course, final Module module) {
+        ItemModel itemModel = new ItemModel(app, app.getJobManager(), app.getDatabaseHelper());
+
+        final ProgressDialog dialog = ProgressDialog.getInstance();
+        dialog.show(activity.getSupportFragmentManager(), ProgressDialog.TAG);
+
+        for (Item item : module.items) {
+            if (item.type.equals(Item.TYPE_VIDEO)) {
+                Result<Item> result = new Result<Item>() {
+                    @Override
+                    protected void onSuccess(Item result, DataSource dataSource) {
+                        Item<VideoItemDetail> video = (Item<VideoItemDetail>) result;
+
+                        if (dataSource == DataSource.NETWORK) {
+                            if (dialog.isAdded()) {
+                                dialog.dismiss();
+                            }
+                            if (sdVideo) {
+                                startDownload(video.detail.stream.sd_url, DownloadModel.DownloadFileType.VIDEO_SD,
+                                        course, module, result);
+                            }
+                            if (hdVideo) {
+                                startDownload(video.detail.stream.hd_url, DownloadModel.DownloadFileType.VIDEO_HD,
+                                        course, module, result);
+                            }
+                            if (slides) {
+                                startDownload(video.detail.slides_url, DownloadModel.DownloadFileType.SLIDES,
+                                        course, module, result);
+                            }
+                        }
+                    }
+                };
+                itemModel.getItemDetail(result, course, module, item, item.type);
+            }
+        }
+
+    }
+
+    private void startDownload(String uri, DownloadModel.DownloadFileType downloadFileType, Course course, Module module, Item item) {
+        if (uri != null
+                && !downloadModel.downloadExists(downloadFileType, course, module, item)
+                && !downloadModel.downloadRunning(downloadFileType, course, module, item)) {
+            downloadModel.startDownload(uri, downloadFileType, course, module, item);
+        }
+    }
+
+}
