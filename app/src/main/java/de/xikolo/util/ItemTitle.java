0896c2e186f1ab8246a3a3b0ecb8a1420cf7b0e4
Tobias Rohloff <tobias.rohloff@student.hpi.uni-potsdam.de>
2016-03-21 21:09:47 +0100

    Bugfix.

--- app/src/main/java/de/xikolo/util/ItemTitle.java
+++ app/src/main/java/de/xikolo/util/ItemTitle.java
@@ -1,10 +1,12 @@
 package de.xikolo.util;
 
+import android.util.Log;
+
 public class ItemTitle {
 
     public static String format(String moduleTitle, String itemTitle) {
         String title;
-        if (itemTitle.length() > moduleTitle.length() &&
+        if (itemTitle.length() > moduleTitle.length() + 2 &&
                 itemTitle.startsWith(moduleTitle) &&
                 itemTitle.substring(moduleTitle.length() + 1, moduleTitle.length() + 2).equals(" ")) {
             title = itemTitle.substring(moduleTitle.length() + 2);
