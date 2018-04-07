9cdb7a32498d385b6eff8ffdc191b200faec739d
Ben Engelhaupt <ben-noah.engelhaupt@student.hpi.de>
2018-04-07 13:35:57 +0200

    Incremented Realm schema version and added migration

--- app/src/main/java/de/xikolo/models/base/RealmSchemaMigration.java
+++ app/src/main/java/de/xikolo/models/base/RealmSchemaMigration.java
@@ -1,6 +1,7 @@
 package de.xikolo.models.base;
 
 import io.realm.DynamicRealm;
+import io.realm.FieldAttribute;
 import io.realm.RealmMigration;
 import io.realm.RealmSchema;
 
@@ -8,11 +9,21 @@ public class RealmSchemaMigration implements RealmMigration {
 
     @Override
     public void migrate(DynamicRealm realm, long oldVersion, long newVersion) {
-        // DynamicRealm exposes an editable schema
         RealmSchema schema = realm.getSchema();
 
-        // Data schema migrations
-        // See https://realm.io/docs/java/latest/#migrations
+        if (oldVersion == 1) {
+            schema.create("Channel")
+                    .addField("id", String.class, FieldAttribute.PRIMARY_KEY)
+                    .addField("name", String.class)
+                    .addField("slug", String.class)
+                    .addField("color", String.class)
+                    .addField("position", int.class)
+                    .addField("description", String.class)
+                    .addField("imageUrl", String.class);
+
+            schema.get("Course")
+                    .addField("channelId", String.class);
+        }
     }
     
 }
