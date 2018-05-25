bf7c20c6eaaef4a9bd929e70225db22b9ae67721
Ben Engelhaupt <ben-noah.engelhaupt@student.hpi.de>
2018-05-26 01:15:36 +0200

    Create settings element

--- app/src/main/java/de/xikolo/controllers/settings/SettingsFragment.kt
+++ app/src/main/java/de/xikolo/controllers/settings/SettingsFragment.kt
@@ -1,6 +1,7 @@
 package de.xikolo.controllers.settings
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -9,6 +10,7 @@ import android.support.v14.preference.PreferenceFragment
 import android.support.v4.content.ContextCompat
 import android.support.v7.preference.Preference
 import android.support.v7.preference.PreferenceCategory
+import android.support.v7.preference.PreferenceManager
 import android.support.v7.preference.PreferenceScreen
 import de.psdev.licensesdialog.LicensesDialog
 import de.xikolo.BuildConfig
@@ -24,7 +26,7 @@ import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 import java.util.*
 
-class SettingsFragment : PreferenceFragment() {
+class SettingsFragment : PreferenceFragment(), SharedPreferences.OnSharedPreferenceChangeListener {
 
     companion object {
         val TAG: String = SettingsFragment::class.java.simpleName
@@ -41,8 +43,28 @@ class SettingsFragment : PreferenceFragment() {
         EventBus.getDefault().register(this)
     }
 
+    override fun onResume() {
+        super.onResume()
+        preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+
+    }
+
+    override fun onPause() {
+        preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+        super.onPause()
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        if (key.equals(getString(R.string.preference_storage)))
+            findPreference(getString(R.string.preference_storage)).summary = sharedPreferences?.getString(getString(R.string.preference_storage), getString(R.string.settings_default_value_storage))
+    }
+
     override fun onCreatePreferences(bundle: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.settings)
+
+        val prefs = PreferenceManager.getDefaultSharedPreferences(activity);
+
+        onSharedPreferenceChanged(prefs, getString(R.string.preference_storage));
 
         if (Build.VERSION.SDK_INT < 23) {
             val screen = findPreference(getString(R.string.preference_screen)) as PreferenceScreen
@@ -94,8 +116,8 @@ class SettingsFragment : PreferenceFragment() {
 
         val buildVersion = findPreference(getString(R.string.preference_build_version))
         buildVersion.summary = (buildVersion.summary.toString()
-                + " "
-                + BuildConfig.VERSION_NAME)
+            + " "
+            + BuildConfig.VERSION_NAME)
 
         val licenses = findPreference(getString(R.string.preference_open_source_licenses))
         licenses.setOnPreferenceClickListener { _ ->
