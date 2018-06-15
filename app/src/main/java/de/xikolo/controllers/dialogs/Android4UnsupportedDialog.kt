d3eb7c3363bcc2afd3b661cf7dc79c29dd679078
Tobias Rohloff <tobias.rohloff@hpi.de>
2018-06-15 17:25:55 +0200

    Deprecate android 4 and communicate it with dialogs

---
+++ app/src/main/java/de/xikolo/controllers/dialogs/Android4UnsupportedDialog.kt
@@ -0,0 +1,36 @@
+package de.xikolo.controllers.dialogs
+
+import android.app.Dialog
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import de.xikolo.R
+import de.xikolo.controllers.dialogs.base.BaseDialogFragment
+
+class Android4UnsupportedDialog : BaseDialogFragment() {
+
+    companion object {
+        val TAG: String = Android4UnsupportedDialog::class.java.simpleName
+    }
+
+    var listener: Listener? = null
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(activity!!, R.style.AppTheme_Dialog)
+            .setTitle(
+                getString(R.string.dialog_android_4_unsupported_title)
+            )
+            .setMessage(
+                getString(R.string.dialog_android_4_unsupported_message)
+            )
+            .setNegativeButton(getString(R.string.dialog_android_4_unsupported_no)) { _, _ ->
+                listener?.onConfirmed()
+            }
+
+        return builder.create()
+    }
+
+    interface Listener {
+        fun onConfirmed()
+    }
+
+}
