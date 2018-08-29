e30c656d1c93e89ec5812dc71ffc5cda1088a728
Ben Engelhaupt <ben-noah.engelhaupt@student.hpi.de>
2018-08-29 18:02:57 +0200

    Remove SubtitleTrack handling and use VideoSubtitles

--- app/src/main/java/de/xikolo/controllers/video/VideoActivity.java
+++ app/src/main/java/de/xikolo/controllers/video/VideoActivity.java
@@ -22,8 +22,6 @@ import com.google.android.gms.cast.framework.CastButtonFactory;
 import com.google.android.gms.cast.framework.CastState;
 import com.yatatsu.autobundle.AutoBundleField;
 
-import java.util.List;
-
 import butterknife.BindView;
 import de.xikolo.R;
 import de.xikolo.controllers.base.BasePresenterActivity;
@@ -31,7 +29,6 @@ import de.xikolo.controllers.helper.VideoHelper;
 import de.xikolo.models.Course;
 import de.xikolo.models.Item;
 import de.xikolo.models.Section;
-import de.xikolo.models.SubtitleTrack;
 import de.xikolo.models.Video;
 import de.xikolo.presenters.base.PresenterFactory;
 import de.xikolo.presenters.video.VideoPresenter;
@@ -41,7 +38,6 @@ import de.xikolo.utils.AndroidDimenUtil;
 import de.xikolo.utils.CastUtil;
 import de.xikolo.utils.LanalyticsUtil;
 import de.xikolo.utils.PlayServicesUtil;
-import de.xikolo.utils.ToastUtil;
 
 public class VideoActivity extends BasePresenterActivity<VideoPresenter, VideoView> implements VideoView {
 
