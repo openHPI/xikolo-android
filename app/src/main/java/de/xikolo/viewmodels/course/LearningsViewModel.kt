615de1dbe6b1f4a2f6f301adea0699e15b6a087b
Ben Engelhaupt <ben-noah.engelhaupt@student.hpi.de>
2019-03-21 00:17:36 +0100

    changed remaining ViewModel

--- app/src/main/java/de/xikolo/viewmodels/course/LearningsViewModel.kt
+++ app/src/main/java/de/xikolo/viewmodels/course/LearningsViewModel.kt
@@ -3,19 +3,16 @@ package de.xikolo.viewmodels.course
 import androidx.lifecycle.LiveData
 import de.xikolo.models.Course
 import de.xikolo.models.Item
-import de.xikolo.models.dao.CoursesDao
 import de.xikolo.models.dao.ItemsDao
 import de.xikolo.network.jobs.ListSectionsWithItemsJob
 import de.xikolo.viewmodels.base.BaseViewModel
 
 open class LearningsViewModel(val courseId: String) : BaseViewModel() {
 
-    private val coursesDao = CoursesDao(realm)
     private val itemsDao = ItemsDao(realm)
+    private val courseViewModel = CourseViewModel(courseId)
 
-    val course: LiveData<Course> by lazy {
-        coursesDao.course(courseId)
-    }
+    val course: LiveData<Course> = courseViewModel.course
 
     val accessibleItems: LiveData<List<Item>> by lazy {
         itemsDao.accessibleItemsForCourse(courseId)
