ea47e23d81fb9e2c5ed7279683b6f67394bfd1ba
Ben Engelhaupt <ben-noah.engelhaupt@student.hpi.de>
2019-08-12 12:45:50 +0200

    refactored ListItemsWithContentJob and fixed GetCourseWithSectionsJob

--- app/src/main/java/de/xikolo/managers/ItemManager.kt
+++ app/src/main/java/de/xikolo/managers/ItemManager.kt
@@ -1,19 +1,13 @@
 package de.xikolo.managers
 
 import de.xikolo.models.Video
-import de.xikolo.network.jobs.ListItemsWithContentForSectionJob
 import de.xikolo.network.jobs.UpdateItemVisitedJob
-import de.xikolo.network.jobs.base.RequestJobCallback
 import io.realm.Realm
 
 class ItemManager {
 
     companion object {
         val TAG: String = ItemManager::class.java.simpleName
-    }
-
-    fun requestItemsWithContentForSection(sectionId: String, callback: RequestJobCallback) {
-        ListItemsWithContentForSectionJob(callback, sectionId).run()
     }
 
     fun updateItemVisited(itemId: String) {
