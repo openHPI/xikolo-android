05e288bb3362954a92e75b22cb3f79452f776aff
Tobias Rohloff <tobias.rohloff@hpi.de>
2020-05-05 16:46:49 +0200

    Update to version 3.3

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -8,8 +8,8 @@ object Versions {
     const val TARGET_SDK = 29
     const val COMPILE_SDK = 29
 
-    const val VERSION_NAME = "3.2.1"
-    const val VERSION_CODE = 54
+    const val VERSION_NAME = "3.3"
+    const val VERSION_CODE = 55
 
     const val XIKOLO_API = 4
     const val REALM_SCHEMA = 13
