ba82dd32a16d1c45382027002d071c940e12c0c4
Ben Engelhaupt <ben-noah.engelhaupt@student.hpi.de>
2020-08-19 13:42:50 +0200

    Add plugins

--- app/src/main/java/de/xikolo/utils/extensions/TextViewExtensions.kt
+++ app/src/main/java/de/xikolo/utils/extensions/TextViewExtensions.kt
@@ -12,9 +12,14 @@ import de.xikolo.config.Config
 import io.noties.markwon.AbstractMarkwonPlugin
 import io.noties.markwon.Markwon
 import io.noties.markwon.core.MarkwonTheme
+import io.noties.markwon.ext.latex.JLatexMathPlugin
+import io.noties.markwon.ext.strikethrough.StrikethroughPlugin
 import io.noties.markwon.ext.tables.TablePlugin
+import io.noties.markwon.ext.tasklist.TaskListPlugin
+import io.noties.markwon.html.HtmlPlugin
 import io.noties.markwon.image.AsyncDrawable
 import io.noties.markwon.image.glide.GlideImagesPlugin
+import io.noties.markwon.inlineparser.MarkwonInlineParserPlugin
 import java.net.MalformedURLException
 import java.net.URI
 import java.net.URISyntaxException
@@ -38,6 +43,12 @@ fun <T : TextView> T.setMarkdownText(markdown: String?) {
 
         val glide = Glide.with(context)
         val formatter = Markwon.builder(context)
+            .usePlugin(MarkwonInlineParserPlugin.create())
+            .usePlugin(JLatexMathPlugin.create(textSize))
+            .usePlugin(StrikethroughPlugin.create())
+            .usePlugin(TablePlugin.create(context))
+            .usePlugin(TaskListPlugin.create(context))
+            .usePlugin(HtmlPlugin.create())
             .usePlugin(
                 GlideImagesPlugin.create(object : GlideImagesPlugin.GlideStore {
                     override fun cancel(target: Target<*>) {
@@ -50,9 +61,6 @@ fun <T : TextView> T.setMarkdownText(markdown: String?) {
                         )
                     }
                 })
-            )
-            .usePlugin(
-                TablePlugin.create(context)
             )
             .usePlugin(
                 object : AbstractMarkwonPlugin() {
