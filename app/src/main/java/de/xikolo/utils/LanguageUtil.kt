652a586b399fa84e47a5afbf3a4eb30ff6db94a5
Sandro Sp <38314662+SanJSp@users.noreply.github.com>
2020-09-10 15:44:05 +0200

    LanguageFilter translation implemented (#273)

--- app/src/main/java/de/xikolo/utils/LanguageUtil.kt
+++ app/src/main/java/de/xikolo/utils/LanguageUtil.kt
@@ -1,18 +1,34 @@
 package de.xikolo.utils
 
+import android.content.Context
 import java.util.Locale
 
 object LanguageUtil {
+    var deviceLanguage: String = Locale.getDefault().language.takeIf { it != "zh" } ?: "cn"
+
     @OptIn(ExperimentalStdlibApi::class)
     fun toNativeName(tag: String): String {
-        val languageTag =
-            if (tag == "cn") { // work-around for bug in the backend
-                "zh"
-            } else {
-                tag
-            }
+        val languageTag = correctLanguageTag(tag)
 
         val locale = Locale.forLanguageTag(languageTag)
         return locale.getDisplayLanguage(locale).capitalize(locale)
+    }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    fun toLocaleName(context: Context, tag: String): String {
+        val languageTag = correctLanguageTag(tag)
+        val locale = Locale.forLanguageTag(languageTag)
+        val deviceLocale = Locale.getDefault()
+
+        return locale.getDisplayLanguage(deviceLocale).capitalize(locale)
+    }
+
+    private fun correctLanguageTag(tag: String): String {
+        // work-around for wrong chinese API locale in backend
+        return if (tag == "cn") {
+            "zh"
+        } else {
+            tag
+        }
     }
 }
