154e782a1dd8a9bfea398827e1f78452eb3400cd
Tobias Rohloff <tobias.rohloff@hpi.de>
2021-06-29 13:24:34 +0200

    Update to version 3.8

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -8,8 +8,8 @@ object Versions {
     const val TARGET_SDK = 30
     const val COMPILE_SDK = 30
 
-    const val VERSION_NAME = "3.7.1"
-    const val VERSION_CODE = 62
+    const val VERSION_NAME = "3.8"
+    const val VERSION_CODE = 63
 
     const val XIKOLO_API = 4
     const val REALM_SCHEMA = 13
