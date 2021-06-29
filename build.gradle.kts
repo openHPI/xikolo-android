c9b7ba142d2434f1d3d21b6213b072c25917b506
Tobias Rohloff <tobias.rohloff@hpi.de>
2021-06-29 12:34:58 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 buildscript {
     repositories {
-        jcenter()
+        mavenCentral()
         google()
     }
     dependencies {
@@ -11,31 +11,27 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.KOTLIN}")
         classpath("io.realm:realm-gradle-plugin:7.0.8")
         classpath("com.google.gms:google-services:4.3.8")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.6.1")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.7.1")
     }
 }
 
 plugins {
-    id("com.github.ben-manes.versions") version "0.38.0"
+    id("com.github.ben-manes.versions") version "0.39.0"
 
     // Make sure to update this in `buildSrc/build.gradle.kts` as well
-    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
+    id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
 }
 
 allprojects {
     repositories {
         jcenter()
+        mavenCentral()
         google()
-        maven("https://jitpack.io")
-        maven("https://clojars.org/repo/")
-        flatDir {
-            dirs("libs")
-        }
     }
 }
 
 ktlint {
-    version.set("0.40.0") // Make sure to update this in `buildSrc/build.gradle.kts` as well
+    version.set("0.41.0") // Make sure to update this in `buildSrc/build.gradle.kts` as well
     android.set(true)
     ignoreFailures.set(true)
     reporters {
