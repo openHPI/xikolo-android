f16a65ba1df654c2a28fdcead4a2ef46ae851e4b
Max Bothe <mathebox@gmail.com>
2022-12-22 14:28:18 +0100

    Update to version 3.9.2

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -8,8 +8,8 @@ object Versions {
     const val TARGET_SDK = 31
     const val COMPILE_SDK = 31
 
-    const val VERSION_NAME = "3.9.1"
-    const val VERSION_CODE = 67
+    const val VERSION_NAME = "3.9.2"
+    const val VERSION_CODE = 68
 
     const val XIKOLO_API = 4
     const val REALM_SCHEMA = 13
